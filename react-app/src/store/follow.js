827099a65cf3bba8d213882b2cccc402a04ab848
bsnernier <nbern817@gmail.com>
2022-08-12 18:09:20 -0500

    add ability to unfollow, fix check if following function to be accurate

--- react-app/src/store/follow.js
+++ react-app/src/store/follow.js
@@ -1,5 +1,6 @@
 const FOLLOW_USER = "session/FOLLOW_USER";
 const CHECK_USER = "session/CHECK_USER";
+const UNFOLLOW_USER = "session/UNFOLLOW_USER";
 
 const followUser = (user) => ({
     type: FOLLOW_USER,
@@ -8,6 +9,11 @@ const followUser = (user) => ({
 
 const checkUser = (user) => ({
     type: CHECK_USER,
+    payload: user,
+});
+
+const unfollowUser = (user) => ({
+    type: UNFOLLOW_USER,
     payload: user,
 });
 
@@ -34,7 +40,31 @@ export const checkAUser = (userId) => async (dispatch) => {
     const res = await fetch(`api/users/${userId}/follow`);
 
     if (res.ok) {
-        dispatch(checkUser(res));
+        const resultDispatch = dispatch(checkUser(res));
+        const result = await resultDispatch.payload.json();
+
+        if (result["Success"]) {
+            return true;
+        } else if (result["Unsuccessful"]) {
+            return false;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+};
+
+export const unfollowAUser = (userId) => async (dispatch) => {
+    const res = await fetch(`api/users/${userId}/unfollow`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    if (res.ok) {
+        dispatch(unfollowUser(res));
         return true;
     } else {
         return false;
@@ -48,6 +78,8 @@ export default function reducer(state = initialState, action) {
         case FOLLOW_USER:
             return { user: action.payload };
         case CHECK_USER:
+            return { user: action.payload };
+        case UNFOLLOW_USER:
             return { user: action.payload };
         default:
             return state;
