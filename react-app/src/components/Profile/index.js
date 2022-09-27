a86f9d80c0ac93212239940f2dbc99ff33c5549e
bsnernier <nbern817@gmail.com>
2022-09-27 07:23:18 -0500

    add filler code for profiles

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -1,7 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPosts } from "../../store/post";
+import Post from "../Post";
 
 function Profile() {
-    return <div>Hello</div>;
+    const dispatch = useDispatch();
+    let list = [];
+
+    useEffect(() => {
+        dispatch(getAllPosts());
+    }, [dispatch]);
+
+    const posts = useSelector((state) => state.post);
+
+    for (let key in posts) {
+        if (key !== "post") {
+            list.push(key);
+        }
+    }
+
+    return (
+        <div className="all-post-container__container">
+            <div className="all-post-container">
+                {list.map((post) => (
+                    <Post postId={post} key={post} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Profile;
